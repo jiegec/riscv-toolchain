9ef30e9f8736ded0159d4d4d25a7663c8c035268
Jiajie Chen <c@jia.je>
2022-01-29 22:37:50 +0800

    Fix tls in gcc

--- scripts/build-gcc-stage2.sh
+++ scripts/build-gcc-stage2.sh
@@ -2,6 +2,6 @@
 cd /work/gcc
 mkdir build
 cd build
-../configure --target=riscv64-unknown-elf --prefix=/work/build --disable-shared --disable-threads --disable-tls --enable-languages=c,c++ --with-system-zlib --with-newlib --with-sysroot=/work/build/riscv64-unknown-elf --disable-libmudflap --disable-libssp --disable-libquadmath --disable-libgomp --disable-nls --disable-tm-clone-registry --disable-multilib --with-abi=lp64d --with-arch=rv64gc
+../configure --target=riscv64-unknown-elf --prefix=/work/build --disable-shared --disable-threads --enable-tls --enable-languages=c,c++ --with-system-zlib --with-newlib --with-sysroot=/work/build/riscv64-unknown-elf --disable-libmudflap --disable-libssp --disable-libquadmath --disable-libgomp --disable-nls --disable-tm-clone-registry --disable-multilib --with-abi=lp64d --with-arch=rv64gc
 make -j8 all
 make install
