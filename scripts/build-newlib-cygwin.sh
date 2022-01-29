2b98ca99ddd0c08af9adbde363148b08c404d33f
Jiajie Chen <c@jia.je>
2022-01-29 22:06:36 +0800

    Add flags from riscv-gnu-toolchain and pin newlib to 4.1.0

--- scripts/build-newlib-cygwin.sh
+++ scripts/build-newlib-cygwin.sh
@@ -3,6 +3,6 @@ export PATH=/work/build/bin:$PATH
 cd /work/newlib-cygwin
 mkdir build
 cd build
-../configure --target=riscv64-unknown-elf --prefix=/work/build --enable-newlib-io-long-double --enable-newlib-io-long-long --enable-newlib-io-c99-formats --enable-newlib-register-fini
+../configure --target=riscv64-unknown-elf --prefix=/work/build --enable-newlib-io-long-double --enable-newlib-io-long-long --enable-newlib-io-c99-formats --enable-newlib-register-fini CFLAGS_FOR_TARGET="-O2 -D_POSIX_MODE" CXXFLAGS_FOR_TARGET="-O2 -D_POSIX_MODE"
 make -j8
 make install
