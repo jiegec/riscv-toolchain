75a188130e62062f02c7b312c77c40383c29487c
Jiajie Chen <c@jia.je>
2022-01-29 20:58:25 +0800

    Add binutils-gdb

---
+++ scripts/build-binutils-gdb.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /work/binutils-gdb
+autoconf
+mkdir build
+cd build
+../configure --target=riscv64-unknown-elf --prefix=/work/build --disable-gdb --disable-sim --disable-libdecnumber --disable-readline
+make -j8
+make install
