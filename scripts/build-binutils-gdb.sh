73f210ecc939413ab84a149ba2fd94e835d0c4e2
Jiajie Chen <c@jia.je>
2022-03-08 14:24:01 +0800

    Enable gdb

--- scripts/build-binutils-gdb.sh
+++ scripts/build-binutils-gdb.sh
@@ -3,6 +3,6 @@ cd /work/binutils-gdb
 autoconf
 mkdir build
 cd build
-../configure --target=riscv64-unknown-elf --prefix=/work/build --disable-gdb --disable-sim --disable-libdecnumber --disable-readline
+../configure --target=riscv64-unknown-elf --prefix=/work/build --enable-gdb --disable-sim --disable-libdecnumber --disable-readline
 make -j8
 make install
