50d5baa82b1ba9517292d84302a7cbec7bfaa848
Jiajie Chen <c@jia.je>
2022-03-09 14:56:34 +0800

    Disable gdbserver

--- scripts/build-binutils-gdb.sh
+++ scripts/build-binutils-gdb.sh
@@ -3,6 +3,6 @@ cd /work/binutils-gdb
 autoconf
 mkdir build
 cd build
-../configure --target=riscv64-unknown-elf --prefix=/work/build --enable-gdb --with-expat --enable-gdbserver
+../configure --target=riscv64-unknown-elf --prefix=/work/build --enable-gdb --with-expat
 make -j8
 make install
